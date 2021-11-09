841cb9e6d1bcdeb2632bf27370de38ddb326f1b2
Madhu shree k <83464443+Madhushree10@users.noreply.github.com>
2021-11-09 10:37:05 +0530

    naïve string matching algo

---
+++ LCS/string matching/naive_strm.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+   char txt[] = "abdhsababafnda";
+   char pat[] = "a";
+   int M = strlen (pat);
+   int N = strlen (txt);
+   for (int i = 0; i <= N - M; i++){
+      int j;
+      for (j = 0; j < M; j++)
+         if (txt[i + j] != pat[j])
+      break;
+      if (j == M)
+         printf ("Pattern matches at index %d \n", i);
+   }
+   return 0;
+}
